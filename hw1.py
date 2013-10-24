f517df5465495c3052a6d3ee7a5ca477b2837afe
aliahm <aliahm18@ccm-2610-d1.evergreen.edu>
2013-10-24 16:45:54 -0700

    yup

---
+++ hw1.py
@@ -0,0 +1,59 @@
+# Ahmed Ali
+# Aliahm18
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math   
+import hw1_test
+
+
+
+
+
+print "Problem 1 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+a = 1
+b = 5.86
+c = 8.5408
+#print (-5.86+math.sqrt(5.86**2-4*1*8.5408))/2
+#print (-5.86-math.sqrt(5.86**2-4*1*8.5408))/2
+print (b+math.sqrt(b**2-4*1*c))/2*a
+print (b-math.sqrt(b**2-4*1*c))/2*a
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+print hw1_test.a
+print hw1_test.b
+print hw1_test.c
+print hw1_test.d
+print hw1_test.e
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+#
+print ((hw1_test.a and hw1_test.b) or (not hw1_test.c) and not (hw1_test.d or hw1_test.e or hw1_test.f))
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
+# Jessica Morton, Haron Yunis, Kahea (Tutor)
+#Reflection:
+# It took me about a good 2hours 30min, The readings definently helped me by understanding variables and how to import variables.
+# From reading Think Python, Chapter 1 & 2 & ICPUP Chapter 1, Section 2.1, Section 4.5
